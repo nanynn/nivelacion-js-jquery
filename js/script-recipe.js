0fa908a2007094dd3d9bba66ba73344193bf8268
Anayn Pavez <nanyn.pavez@gmail.com>
2017-07-10 17:05:28 -0300

    Etapa 8: Se realizaron dos funciones que se vinculan con el boton show-recipe y el boton show-make estas funciones se activa con click y cuando se apreta show-recipe se remueve la clase make y con el boton show-make se suma la clase make. Estan en funciones fuera y se llaman dentro del ready

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -1,3 +1,19 @@
 $(document).ready(function() {
 	console.log('hola mundo');
+	showRecipe();
+	showMake();
+	
 });
+
+
+function showRecipe(){
+	$('.js-show-recipe').click(function() {
+		$('#contenedor').removeClass('make');
+	});
+}
+
+function showMake(){
+	$('.js-show-make').click(function() {
+		$('#contenedor').addClass('make');
+	});
+}
