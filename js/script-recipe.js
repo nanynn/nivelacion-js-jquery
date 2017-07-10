88d2cd9d49986d14ef678153203c812427184167
Anayn Pavez <nanyn.pavez@gmail.com>
2017-07-10 17:26:35 -0300

    Etapa 9: le dije que cuando el boton sea show-recipe quite la clase active y cuando eso pase le ponga la clase active a show-make en la funcion showRecipe y a la version showMake hice los contrario

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -5,15 +5,21 @@ $(document).ready(function() {
 	
 });
 
-
 function showRecipe(){
 	$('.js-show-recipe').click(function() {
 		$('#contenedor').removeClass('make');
+		$('.js-show-recipe').removeClass('active');
+		$('.js-show-make').addClass('active');
+		
+		
 	});
 }
 
 function showMake(){
 	$('.js-show-make').click(function() {
 		$('#contenedor').addClass('make');
+		
+		$('.js-show-recipe').addClass('active');
+		$('.js-show-make').removeClass('active');
 	});
 }
